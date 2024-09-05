7508e89110868cbfb6c9ff235c4035ee074c2ac1
kkjsawantucla <166880350+kkjsawantucla@users.noreply.github.com>
2024-09-05 11:42:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="gg",
     version=0.1,
     description="GG is an open-source code for building graph based grand canonical basin hopping calculators",
-    download_url="https://github.com/kkjsawantucla/gg.git",
+    download_url="https://github.com/kkjsawantucla/gg",
     author="Kaustubh Sawant, Geng Sun",
     python_requires=">=3.7",
     packages=find_packages(include=["gg", "gg.*"]),
